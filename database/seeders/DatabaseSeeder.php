fbfcd73d8f1ceb6fbc4bd6549732f04f7120e657
amirrajabzadeh <developer908908@gmail.com>
2025-11-27 13:13:52 +0330

    add controllers, routes and create seed for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+//         User::factory(10)->create();
 
-        User::factory()->create([
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);*/
+
+        //ایجاد کاربر مدیر
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@tasksflow.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
         ]);
+
+        //ایجاد کاربر رهبر تیم
+        User::create([
+            'name' => 'Team Leader',
+            'email' => 'teamleader@tasksflow.com',
+            'password' => Hash::make('password'),
+            'role' => 'team_leader',
+            'email_verified_at' => now(),
+        ]);
+
+        //ایجاد 10 کاربر کارمند
+        $workers=[
+            ['name' => 'Worker1','email' => 'worker1@tasksflow.com'],
+            ['name' => 'Worker2','email' => 'worker2@tasksflow.com'],
+            ['name' => 'Worker3','email' => 'worker3@tasksflow.com'],
+            ['name' => 'Worker4','email' => 'worker4@tasksflow.com'],
+            ['name' => 'Worker5','email' => 'worker5@tasksflow.com'],
+            ['name' => 'Worker6','email' => 'worker6@tasksflow.com'],
+            ['name' => 'Worker7','email' => 'worker7@tasksflow.com'],
+            ['name' => 'Worker8','email' => 'worker8@tasksflow.com'],
+            ['name' => 'Worker9','email' => 'worker9@tasksflow.com'],
+            ['name' => 'Worker10','email' => 'worker10@tasksflow.com'],
+        ];
+
+        foreach ($workers as $worker) {
+            User::create([
+                'name' => $worker['name'],
+                'email' => $worker['email'],
+                'password' => Hash::make('password'),
+                'role' => 'worker',
+                'email_verified_at' => now(),
+            ]);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('dashboard', DashboardController::class);
+
+Route::resource('projects', ProjectController::class)->middleware('role:admin,team_leader');
+
+Route::resource('tasks', TaskController::class);
+
+Route::post('tasks/{task}/update-status', [TaskController::class, 'updateStatus'])->name('tasks.update-status');
